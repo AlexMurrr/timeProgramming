5f5a3d13e235656f362ad643735909739179836f
Alex Mur <lehamuraschov2014@yandex.ru>
2022-10-23 07:45:12 +0200

    feat: first insert into db table

--- index.js
+++ index.js
@@ -4,14 +4,28 @@ const connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : 'AAAAAAA',
-    database : 'foo'
-  });
+    database : 'timeprogramming'
+  });   
 
-  connection.query("SELECT * FROM triangle;",
-  function(err, results, fields) {
-    console.log(err);
-    console.log(results); // собственно данные    
-});
-connection.end();
+//   connection.query("SELECT * FROM ;",
+//   function(err, results, fields) {
+//     console.log(err);
+//     console.log(results); // собственно данные    
+// });
+// connection.end();
 
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL Server!');
+  }); 
+
+//console.log(nowMiliSeconds);
+
+let nowMiliSeconds = Date.now();
   
+const sql = 'INSERT INTO time (timestamp) VALUES(?)';
+
+connection.query(sql, [nowMiliSeconds], (err, res)=>{
+    if(err) console.log(err.message);
+    console.log('Add successfull');
+})
