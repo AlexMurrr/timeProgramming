0ddb734ff3da100d9a3cb2e3884410e0ef1a39fe
Alex Mur <lehamuraschov2014@yandex.ru>
2022-10-26 22:23:08 +0200

    feat: install nodemod and write 'dev'

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-const mysql = require('mysql2');
+//const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'AAAAAAA',
-    database : 'timeprogramming'
-  });   
+// const connection = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : 'AAAAAAA',
+//     database : 'timeprogramming'
+//   }); 
 
-//   connection.query("SELECT * FROM ;",
-//   function(err, results, fields) {
-//     console.log(err);
-//     console.log(results); // собственно данные    
-// });
-// connection.end();
-
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to MySQL Server!');
-  }); 
-
-//console.log(nowMiliSeconds);
-
-let nowMiliSeconds = Date.now();
+// let nowMiliSeconds = Date.now();
   
-const sql = 'INSERT INTO time (timestamp) VALUES(?)';
+// const sql = 'INSERT INTO time (timestamp) VALUES(?)';  
 
-connection.query(sql, [nowMiliSeconds], (err, res)=>{
-    if(err) console.log(err.message);
-    console.log('Add successfull');
-})
+// function queryInsert(){
+// connection.connect((err) => {
+//     if (err) throw err;
+//     console.log('Connected to MySQL Server!');
+//   }); 
+// connection.query(sql, nowMiliSeconds, (err, res)=>{
+//     if(err) console.log(err.message);
+//     console.log('Add successfull');
+// })}
+
+function a (){
+    console.log('yes')
+}
+
+let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener('click', a());
