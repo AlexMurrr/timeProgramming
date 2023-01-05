b07eb0e9841f1642aa9cd503b3d91602e9bae14a
Alex Mur <lehamuraschov2014@yandex.ru>
2023-01-05 21:54:57 +0200

    feat: created a function that returns the string name of the day of the week instead of a number

--- formatDate.js
+++ formatDate.js
@@ -1,8 +1,38 @@
 let date = new Date();
 
+function getDayFromNumber (num){
+    let dayWeek='';
+    switch(num){
+        case 0:
+            dayWeek = 'sunday';
+            break;
+        case 1:
+            dayWeek = 'monday';
+            break;    
+        case 2:
+            dayWeek = 'tuesday';
+            break; 
+        case 3:
+            dayWeek = 'wednesday';
+            break;  
+        case 4:
+            dayWeek = 'thursday';
+            break;     
+        case 5:
+            dayWeek = 'friday';
+            break;  
+        case 6:
+            dayWeek = 'saturday';
+            break;                      
+    }
+
+    return dayWeek;
+
+}
+
 function getArrDate (){
     return [date.getFullYear(), date.getMonth() + 1, date.getDate(), 
-            date.getDay()]
+        getDayFromNumber(date.getDay())]
 }
 
 exports.getArrDate = getArrDate;
