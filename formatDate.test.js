a51c27ac7ecedbd59c1a559616bf2baddd73b730
Alex Mur <lehamuraschov2014@yandex.ru>
2023-01-09 23:37:22 +0200

    test: check function getArrDate test: is arr

--- formatDate.test.js
+++ formatDate.test.js
@@ -1,6 +1,7 @@
 const formateDate = require("./formatDate");
 
 const getDayFromNumber = formateDate.getDayFromNumber;
+const getArrDate = formateDate.getArrDate;
 
 describe('check function getDayFromNumber', ()=>{
     test('return sunday', () => {
@@ -9,4 +10,10 @@ describe('check function getDayFromNumber', ()=>{
     test('return saturday', () => {
         expect(getDayFromNumber(6)).toBe('saturday');
     })  
+})
+
+describe('check function getArrDate', ()=>{
+    test('is arr', ()=>{
+        expect(Array.isArray(getArrDate())).toBeTruthy();
+    });
 })
