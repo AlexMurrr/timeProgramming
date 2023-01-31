9ab9f8370983bf3e457298553c98d2b60bb4dd31
Alex Mur <lehamuraschov2014@yandex.ru>
2023-02-01 00:25:14 +0200

    feat: create MY FIRST CHART

--- public/charts.js
+++ public/charts.js
@@ -1,42 +1,42 @@
 //import getSumTime from '../formatDate';
 
 const ctx = document.getElementById("myChart").getContext("2d");
-const myChart = new Chart(ctx, {
-  type: "pie",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  },
-});
+// const myChart = new Chart(ctx, {
+//   type: "pie",
+//   data: {
+//     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//     datasets: [
+//       {
+//         label: "# of Votes",
+//         data: [12, 19, 3, 5, 2, 3],
+//         backgroundColor: [
+//           "rgba(255, 99, 132, 0.2)",
+//           "rgba(54, 162, 235, 0.2)",
+//           "rgba(255, 206, 86, 0.2)",
+//           "rgba(75, 192, 192, 0.2)",
+//           "rgba(153, 102, 255, 0.2)",
+//           "rgba(255, 159, 64, 0.2)",
+//         ],
+//         borderColor: [
+//           "rgba(255, 99, 132, 1)",
+//           "rgba(54, 162, 235, 1)",
+//           "rgba(255, 206, 86, 1)",
+//           "rgba(75, 192, 192, 1)",
+//           "rgba(153, 102, 255, 1)",
+//           "rgba(255, 159, 64, 1)",
+//         ],
+//         borderWidth: 1,
+//       },
+//     ],
+//   },
+//   options: {
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//       },
+//     },
+//   },
+// });
 
 let sumTime = document.getElementById('sumTime');
 let sumMusic = document.getElementById('sumMusic');  
@@ -50,8 +50,37 @@ async function getData(){
     const resMusic = await fetch('http://localhost:8080/countMusic')
     const time = await res.json();
     const timeMusic = await resMusic.json();
-    sumTime.innerHTML += getSumTime(time.data);
-    sumMusic.innerHTML += getSumTime(timeMusic.data);
+    sumTime.innerHTML += (getSumTime(time.data) + ' programming');
+    sumMusic.innerHTML += (getSumTime(timeMusic.data) + ' music');
+
+    const myChart = new Chart(ctx, {
+      type: "pie",
+      data: {
+        labels: ["Programming", "Music"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [getSumTime(time.data), getSumTime(timeMusic.data)],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)"              
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)"
+            ],
+            borderWidth: 3,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
 }
 
 
