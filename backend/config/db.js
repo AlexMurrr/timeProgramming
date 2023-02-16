cf7863f1f8e55af5efdbeb439182cb93f1262d8d
Alex Mur <lehamuraschov2014@yandex.ru>
2023-02-16 14:00:13 +0200

    feat: change requaer to type: modules

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,12 @@
-const sqlite3 = require("sqlite3").verbose();
-const express = require("express");
-const app = express();
+import sqlite3 from "sqlite3";
 
 const dataSource = "timeOfProg.db";
 
-let db = new sqlite3.Database(dataSource, (err) => {
+const db = new sqlite3.Database(dataSource, (err) => {
   if (err) {
     return console.error(err.message);
   }
   console.log("Connected to the in-memory SQlite database.");
 });
 
-module.exports = db;
+export default db;
