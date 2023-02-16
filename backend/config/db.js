dd5f6d872d40bc0fbed577eec8aa671f9f0758c4
Alex Mur <lehamuraschov2014@yandex.ru>
2023-02-16 13:27:42 +0200

    feat: create connect to timeOfProg.db file db.js in folder config

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const sqlite3 = require("sqlite3").verbose();
+const express = require("express");
+const app = express();
+
+const dataSource = "timeOfProg.db";
+
+let db = new sqlite3.Database(dataSource, (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connected to the in-memory SQlite database.");
+});
+
+module.exports = db;
